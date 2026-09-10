c1a5d82670d72d6fb8aff15e84d6920e9506f41d
Paulo Kositis <paulokositis@gmail.com>
2026-09-10 18:41:21 -0300

    Resultado criado, resolução feita.

---
+++ 3/desafio.py
@@ -0,0 +1,11 @@
+distancia = int(input("Quantos metros você percorreu? "))
+
+tempo = int(input("Isso em quanto tempo (escreva em segundos)?"))
+
+velocidade_media = distancia / tempo
+
+print(f"A velocidade é de {velocidade_media}m/s")
+
+velocidade_por_km = velocidade_media * 3.6
+
+print(f"A velocidade atingida por km é de {velocidade_por_km}km/h")
