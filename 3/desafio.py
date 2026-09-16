a912dc9a2411d894d7bfd3a7c14fecd4f45f3f72
Paulo Kositis <paulokositis@gmail.com>
2026-09-15 22:02:11 -0300

    Lógica adicionada

--- 3/desafio.py
+++ 3/desafio.py
@@ -1,11 +1,11 @@
 distancia = int(input("Quantos metros você percorreu? "))
 
-tempo = int(input("Isso em quanto tempo (escreva em segundos)?"))
+tempo = int(input("Isso em quanto tempo (escreva em segundos, sem pontos ou virgulas)? "))
 
 velocidade_media = distancia / tempo
 
-print(f"A velocidade é de {velocidade_media}m/s")
+print(f"A velocidade é de {velocidade_media} m/s")
 
 velocidade_por_km = velocidade_media * 3.6
 
-print(f"A velocidade atingida por km é de {velocidade_por_km}km/h")
+print(f"A velocidade atingida por km é de {velocidade_por_km} km/h")
